1294c546975ec036955402128fc86a68406ad278
federicotonegatti-UNIBO <federico.tonegatti@gmail.com>
2026-04-15 17:35:08 +0200

    Add libraries and plot satellite data in R script

--- Code/06_exam_example.R
+++ Code/06_exam_example.R
@@ -1,3 +1,21 @@
 
-setwd("CimageRysetwd("C:/Users/utente/Desktop/Assegno UNIBO/corsi e schools/TELERILEVAMENTO")
+library(terra)
+library(imageRy)
+library(viridis)
+
+setwd("C:/Users/utente/Desktop/Assegno UNIBO/corsi e schools/TELERILEVAMENTO")
 getwd()
+
+list.files()
+
+sat<-rast("sentinel2_median_2020.tif")
+
+plot(sat[[3]])
+
+par(mfrow=c(2,1))
+plot(sat[[1]])
+plot(sat[[2]])
+
+par(mfrow=c(1,2))
+hist(values(sat[[1]]), main="istogramma 1", col="blue")
+hist(values(sat[[2]]), main="istogramma 1", col="red")
