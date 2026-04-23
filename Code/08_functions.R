f5fb5f718ab02c09bfe007822363312f546538a7
federicotonegatti-UNIBO <federico.tonegatti@gmail.com>
2026-04-23 15:43:26 +0200

    Update 08_functions.R

--- Code/08_functions.R
+++ Code/08_functions.R
@@ -1,1 +1,25 @@
 
+#sviluppo le mie funzioni
+#esempio. funzione somma. un afunzione nuova si usa la funzione function(). definisco gli argomenti della funzione
+#devo aprire le parentesi {}
+#return() funzione per chiudere la funzione
+
+somma <-function(x,y){
+  z=x+y
+  return(z)
+}
+
+#ora posso usare la funzione somma!
+
+somma(1,100)
+
+#creo la funizone differenza
+
+differenza <-function(x,y){
+  z=x-y
+  return(z)
+}
+
+differenza(8,12)
+
+#costrutti if e else...se succede qualcosa, allora fai questo; se succede questo, fai quest'altro
